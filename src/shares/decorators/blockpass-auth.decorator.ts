a9da2bd79cc752805e827e9a02fce1c3403041ff
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-04-05 18:04:34 +0700

    Add BlockPassAuth decorator

---
+++ src/shares/decorators/blockpass-auth.decorator.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { createHmac } from 'crypto';
+import { InvalidSignatureException } from '../exceptions/users.exceptions';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+export class BlockPassAuth implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const body = request.body();
+    const signature = request.headers['x-hub-signature'];
+    const hash = createHmac('sha256', process.env.BLOCK_PASS_SECRET_KEY).update(JSON.stringify(body)).digest('hex');
+    if (!signature || signature !== hash) {
+      throw new InvalidSignatureException(`BlockPass auth error`);
+    }
+    return true;
+  }
+}
