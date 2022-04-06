3a669ef77877779d01f9a318535e229bfd83e60c
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-04-06 14:30:31 +0700

    Declare UsersService in users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Users, UsersSchema } from '../../schemas/users.schema';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }])],
   controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
