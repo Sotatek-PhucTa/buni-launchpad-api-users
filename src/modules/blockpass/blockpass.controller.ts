5d38ca03fa4050a779612ca191b8d202e805eee5
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-04-06 14:40:09 +0700

    Add blockpass receive api

--- src/modules/blockpass/blockpass.controller.ts
+++ src/modules/blockpass/blockpass.controller.ts
@@ -1,8 +1,16 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BlockPassAuth } from '../../shares/decorators/blockpass-auth.decorator';
+import { BlockPassUpdateStatusRequestDto } from './dto/blockpass-update-status.request.dto';
+import { BlockPassService } from './blockpass.service';
 
 @ApiTags('block-pass')
 @UseGuards(BlockPassAuth)
 @Controller('block-pass')
-export class BlockPassController {}
+export class BlockPassController {
+  constructor(private readonly blockPassService: BlockPassService) {}
+  @Post('receive')
+  async receiveUpdateStatus(param: BlockPassUpdateStatusRequestDto) {
+    await this.blockPassService.kycUpdateStatus(param);
+  }
+}
--- src/modules/blockpass/blockpass.module.ts
+++ src/modules/blockpass/blockpass.module.ts
@@ -3,9 +3,11 @@ import { BlockPassController } from './blockpass.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlockPass, BlockPassSchema } from '../../schemas/blockpass.schema';
 import { UsersModule } from '../users/users.module';
+import { BlockPassService } from './blockpass.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: BlockPass.name, schema: BlockPassSchema }]), UsersModule],
   controllers: [BlockPassController],
+  providers: [BlockPassService],
 })
 export class BlockPassModule {}
