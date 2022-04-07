bfb4372fa9fdd137eacdfed07d04e668ccb2bf0e
hoangphuc <hoangphucnb97@gmail.com>
2022-04-07 17:39:18 +0700

    Use dto folder

--- src/modules/users/dto/users.dto.ts
+++ src/modules/users/dto/users.dto.ts
@@ -1,6 +1,6 @@
 import { Exclude, Expose, Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { convertToChecksumAddress } from '../../shares/utils/web3.util';
+import { convertToChecksumAddress } from '../../../shares/utils/web3.util';
 
 @Exclude()
 export class UserLoginRequestDto {
