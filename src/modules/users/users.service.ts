41551e7c8a84d482221f695c1e487fdb8cfad9da
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-04-07 14:08:59 +0700

    Update method findAnUser

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,16 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Users, UsersDocument } from '../../schemas/users.schema';
 import { Model } from 'mongoose';
 import { UsersStatus } from '../../shares/enums/users.enum';
-import { BlockPassUpdateStatusRequestDto } from '../blockpass/dto/blockpass-update-status.request.dto';
 import { KYC_STATUS } from '../../shares/enums/blockpass.enum';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(Users.name) private readonly usersModel: Model<UsersDocument>) {}
-  async findAnActiveUser(wallet_address: string): Promise<UsersDocument> {
+  async findAnUser(wallet_address: string): Promise<UsersDocument> {
     return this.usersModel.findOne({
       wallet_address,
-      status: UsersStatus.ACTIVE,
     });
   }
 
@@ -39,5 +37,9 @@ export class UsersService {
       },
       { upsert: true },
     );
+  }
+
+  async getProfile(userAddress: string) {
+
   }
 }
